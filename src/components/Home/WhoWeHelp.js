9ca3de05f80283493bf5862959049a3401f3f1fb
Grzegorz Kaczor <grzesiek125@op.pl>
2019-12-15 13:25:08 +0100

    whoWeHelpJson

--- src/components/Home/WhoWeHelp.js
+++ src/components/Home/WhoWeHelp.js
@@ -6,12 +6,22 @@ import {
     Switch,
     NavLink,
 } from 'react-router-dom';
+import decoration from '../../assets/Decoration.svg';
 
 
 function WhoWeHelp() {
     return <HashRouter>
         <>
-            <h3>WhoWeHelp</h3>
+            <section className={'whoWeHelp'}>
+                <h1>Komu pomagamy?</h1>
+                <img className={'decoration'} src={decoration}></img>
+
+                <div className={'who flex-box'}>
+                    <p>Fundacjom</p>
+                    <p>Organizacjom pozarządowym</p>
+                    <p>Lokalnym <br/>zbiórkom</p>
+                </div>
+            </section>
 
         </>
     </HashRouter>;
