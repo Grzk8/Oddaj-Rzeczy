9bdd051cd8efbdd8759d885c63281983a11262ef
Grzegorz Kaczor <grzesiek125@op.pl>
2019-12-18 19:38:53 +0100

    whoWeHelp_list

--- src/components/Home/WhoWeHelp.js
+++ src/components/Home/WhoWeHelp.js
@@ -3,18 +3,27 @@ import {
     HashRouter
 } from 'react-router-dom';
 import decoration from '../../assets/Decoration.svg';
+
 import organizations from '../../Data/whoWeHelp';
 
 class WhoWeHelp extends Component {
-    constructor(props) {
-        
-        };
+
+        state= {
+            org: 1,
+            currentPage: 1,
+            listPerPage: 3,
+        }
 
 
-   //   handleClick = name => () => this.state(name);
+        handleClick = (currentorg) =>{
+            this.setState({
+                org: currentorg
+            })
+}
     render() {
+        console.log(organizations.organizations[1].description);
 
-
+        const orgn = organizations.organizations;
         return <HashRouter>
             <>
                 <section className={'whoWeHelp'} id='WhoWeHelp'>
@@ -22,10 +31,22 @@ class WhoWeHelp extends Component {
                     <img className={'decoration'} src={decoration}></img>
 
                     <div className={'who flex-box'}>
-                        <p onClick={this.handleClick('Fundacjom')}>Fundacjom</p>
-                        <p onClick={this.handleClick('Organizacjom pozarządowym')}>Organizacjom pozarządowym</p>
-                        <p onClick={this.handleClick('Zbiórkom lokamnym')}>Lokalnym <br/>zbiórkom</p>
+                        <p onClick={() => this.handleClick(0)}>{orgn[0].navName}</p>
+                        <p onClick={() => this.handleClick(1)}>{orgn[1].navName}</p>
+                        <p onClick={() => this.handleClick(2)}>{orgn[2].navName}</p>
                     </div>
+                    <p className={'description'}>{orgn[this.setState.org].description}</p>
+                    <div>
+                        list.map(element=>(
+                        <tr>
+                            <td>{orgn.name}
+                            <p>{orgn.mission}</p>
+                            </td>
+                            <td>{orgn.things}</td>
+                        </tr>
+                        )
+                    </div>
+                    <div>{orgn.things}</div>
                 </section>
 
             </>
