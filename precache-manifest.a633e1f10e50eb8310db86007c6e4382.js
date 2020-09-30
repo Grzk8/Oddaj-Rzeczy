a8c8bb63db17759b8040d9c9e7f1f3ba870da41e
Grzegorz Kaczor <grzesiek125@op.pl>
2020-09-30 22:44:35 +0200

    Updates

---
+++ precache-manifest.a633e1f10e50eb8310db86007c6e4382.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d64d19a102a66409847fe0293b93ccfb",
+    "url": "/Oddaj-Rzeczy/index.html"
+  },
+  {
+    "revision": "ecf74aae643dda3d6c7e",
+    "url": "/Oddaj-Rzeczy/static/css/main.d6e17c52.chunk.css"
+  },
+  {
+    "revision": "c0b915669d21ca898326",
+    "url": "/Oddaj-Rzeczy/static/js/2.870b30f0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Oddaj-Rzeczy/static/js/2.870b30f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecf74aae643dda3d6c7e",
+    "url": "/Oddaj-Rzeczy/static/js/main.bbd82689.chunk.js"
+  },
+  {
+    "revision": "0ae9634ed9287450c07e",
+    "url": "/Oddaj-Rzeczy/static/js/runtime-main.3d848154.js"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/Oddaj-Rzeczy/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/Oddaj-Rzeczy/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/Oddaj-Rzeczy/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/Oddaj-Rzeczy/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/Oddaj-Rzeczy/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/Oddaj-Rzeczy/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/Oddaj-Rzeczy/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/Oddaj-Rzeczy/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/Oddaj-Rzeczy/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/Oddaj-Rzeczy/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/Oddaj-Rzeczy/static/media/Signature.e54cfcfc.svg"
+  }
+]);
